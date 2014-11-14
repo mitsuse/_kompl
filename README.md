259cc750a33f420198841c87e7cd6ad0e19fadbe
Tomoya Kose <tomoya@mitsuse.jp>
2014-11-15 08:15:39 +0900

    Initialize this repository.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Compl
+
+[![Build Status](https://travis-ci.org/mitsuse/compl.svg?branch=master)](https://travis-ci.org/mitsuse/compl)
+[![GoDoc](http://godoc.org/github.com/mitsuse/compl?status.svg)](http://godoc.org/github.com/mitsuse/compl)
+
+A server for N-gram based word completion implemented in Golang.
---
+++ cmd/compl/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := initApp()
+	app.Run(os.Args)
+}
+
+func initApp() *cli.App {
+	app := cli.NewApp()
+
+	app.Name = "compl"
+	app.Version = "0.0.1"
+	app.Usage = "A server for N-gram based word completion implemented in Golang."
+
+	return app
+}
