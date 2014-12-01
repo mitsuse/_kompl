c0c3c3fa5cd1d7c1fc88cba094252960e69604ed
Tomoya Kose <tomoya@mitsuse.jp>
2014-12-01 21:03:29 +0900

    Deserialize the trie into a file.

--- trie/trie.go
+++ trie/trie.go
@@ -1,6 +1,7 @@
 package trie
 
 import (
+	"encoding/binary"
 	"io"
 	"sort"
 )
@@ -22,7 +23,50 @@ func New() (t *Trie) {
 }
 
 func (t *Trie) Deflate(writer io.Writer) error {
-	// TODO: Write this trie into writer.
+	nodeStack := []*Trie{t}
+	offsetStack := []int{-1}
+
+	if err := t.deflateNode(writer); err != nil {
+		return err
+	}
+
+	for len(nodeStack) > 0 {
+		offsetStack[len(offsetStack)-1]++
+		node := nodeStack[len(nodeStack)-1]
+		offset := offsetStack[len(offsetStack)-1]
+
+		if offset < node.childSeq.Len() {
+			child := node.childSeq[offset]
+			if err := child.deflateNode(writer); err != nil {
+				return err
+			}
+
+			nodeStack = append(nodeStack, child)
+			offsetStack = append(offsetStack, -1)
+		} else {
+			nodeStack = nodeStack[:len(nodeStack)-1]
+			offsetStack = offsetStack[:len(offsetStack)-1]
+		}
+	}
+
+	return nil
+}
+
+func (t *Trie) deflateNode(writer io.Writer) error {
+	endian := binary.LittleEndian
+
+	if err := binary.Write(writer, endian, t.char); err != nil {
+		return err
+	}
+
+	if err := binary.Write(writer, endian, int64(t.Value)); err != nil {
+		return err
+	}
+
+	if err := binary.Write(writer, endian, int64(len(t.childSeq))); err != nil {
+		return err
+	}
+
 	return nil
 }
 
