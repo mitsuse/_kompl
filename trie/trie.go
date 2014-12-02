a827643b570ed0c65358d88e195aeca048d5eb45
Tomoya Kose <tomoya@mitsuse.jp>
2014-12-02 21:48:22 +0900

    Inflate a trie from "io.Reader".

--- trie/trie.go
+++ trie/trie.go
@@ -23,8 +23,64 @@ func New() (t *Trie) {
 }
 
 func Inflate(reader io.Reader) (*Trie, error) {
-	// TODO: Implement this.
-	return nil, nil
+	t, err := inflateNode(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeStack := []*Trie{t}
+	offsetStack := []int{0}
+
+	for len(nodeStack) > 0 {
+		node := nodeStack[len(nodeStack)-1]
+		offset := offsetStack[len(offsetStack)-1]
+
+		if offset < node.childSeq.Len() {
+			child, err := inflateNode(reader)
+			if err != nil {
+				return nil, err
+			}
+
+			node.childSeq[offset] = child
+			offset++
+			offsetStack[len(offsetStack)-1] = offset
+
+			nodeStack = append(nodeStack, child)
+			offsetStack = append(offsetStack, 0)
+		} else {
+			nodeStack = nodeStack[:len(nodeStack)-1]
+			offsetStack = offsetStack[:len(offsetStack)-1]
+		}
+	}
+
+	return t, nil
+}
+
+func inflateNode(reader io.Reader) (*Trie, error) {
+	endian := binary.LittleEndian
+
+	var char int32
+	if err := binary.Read(reader, endian, &char); err != nil {
+		return nil, err
+	}
+
+	var value int64
+	if err := binary.Read(reader, endian, &value); err != nil {
+		return nil, err
+	}
+
+	var childSeqSize int64
+	if err := binary.Read(reader, endian, &childSeqSize); err != nil {
+		return nil, err
+	}
+
+	t := &Trie{
+		char:     char,
+		childSeq: make([]*Trie, childSeqSize),
+		Value:    int(value),
+	}
+
+	return t, nil
 }
 
 func (t *Trie) Deflate(writer io.Writer) error {
