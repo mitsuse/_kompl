d513147e94fcd8a56a7122b4ff89012a5fd62544
Tomoya Kose <tomoya@mitsuse.jp>
2014-12-11 08:55:03 +0900

    Change the message for error occured on running the server.

--- cmd/kompl/command/message.go
+++ cmd/kompl/command/message.go
@@ -2,5 +2,5 @@ package command
 
 const (
 	ERROR_LOADING_PREDICTOR = "failed to load a predictor"
-	ERROR_RUNNING_SERVER    = "failed to run the Kompl server"
+	ERROR_RUNNING_SERVER    = "failed to run the server"
 )
