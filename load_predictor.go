6d8d19bbd35e19cbd9c99e0be59f7819f44c7f20
Tomoya Kose <tomoya@mitsuse.jp>
2014-12-23 02:25:13 +0900

    Split "LoadPredictor" into three function: "loadValueSeq", "loadValue" and "LoadPredictor" itself.

--- load_predictor.go
+++ load_predictor.go
@@ -24,41 +24,9 @@ func LoadPredictor(reader io.Reader) (*Predictor, error) {
 		return nil, err
 	}
 
-	var valueSeqSize int64
-	if err := binary.Read(reader, binary.LittleEndian, &valueSeqSize); err != nil {
+	valueSeq, err := loadValueSeq(reader)
+	if err != nil {
 		return nil, err
-	}
-
-	valueSeq := make([]*Value, valueSeqSize)
-	for i := 0; i < len(valueSeq); i++ {
-		var count int64
-		var maxCount int64
-		var first int64
-		var sibling int64
-
-		if err := binary.Read(reader, binary.LittleEndian, &count); err != nil {
-			return nil, err
-		}
-
-		if err := binary.Read(reader, binary.LittleEndian, &maxCount); err != nil {
-			return nil, err
-		}
-
-		if err := binary.Read(reader, binary.LittleEndian, &first); err != nil {
-			return nil, err
-		}
-
-		if err := binary.Read(reader, binary.LittleEndian, &sibling); err != nil {
-			return nil, err
-		}
-
-		value := &Value{
-			Count:    int(count),
-			MaxCount: int(maxCount),
-			First:    int(first),
-			Sibling:  int(sibling),
-		}
-		valueSeq[i] = value
 	}
 
 	p := &Predictor{
@@ -69,4 +37,55 @@ func LoadPredictor(reader io.Reader) (*Predictor, error) {
 	}
 
 	return p, nil
+}
+
+func loadValueSeq(reader io.Reader) ([]*Value, error) {
+	var valueSeqSize int64
+	if err := binary.Read(reader, binary.LittleEndian, &valueSeqSize); err != nil {
+		return nil, err
+	}
+
+	valueSeq := make([]*Value, valueSeqSize)
+	for i := 0; i < len(valueSeq); i++ {
+		value, err := loadValue(reader)
+		if err != nil {
+			return nil, err
+		}
+
+		valueSeq[i] = value
+	}
+
+	return valueSeq, nil
+}
+
+func loadValue(reader io.Reader) (*Value, error) {
+	var count int64
+	var maxCount int64
+	var first int64
+	var sibling int64
+
+	if err := binary.Read(reader, binary.LittleEndian, &count); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(reader, binary.LittleEndian, &maxCount); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(reader, binary.LittleEndian, &first); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(reader, binary.LittleEndian, &sibling); err != nil {
+		return nil, err
+	}
+
+	value := &Value{
+		Count:    int(count),
+		MaxCount: int(maxCount),
+		First:    int(first),
+		Sibling:  int(sibling),
+	}
+
+	return value, nil
 }
