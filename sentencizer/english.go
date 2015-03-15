6ff11195c62d831dc09dd17c5aedf8414f3cb0e4
Tomoya Kose <tomoya@mitsuse.jp>
2015-03-15 22:02:05 +0900

    Implement a stupid sentencizer for English. #7

---
+++ sentencizer/english.go
@@ -0,0 +1,43 @@
+package sentencizer
+
+import (
+	"regexp"
+)
+
+const (
+	_EN_DOT_EOS_PATTERN    = `^[\.]$`
+	_EN_SYMBOL_EOS_PATTERN = `^[!\?]+$`
+)
+
+type EnglishSentencizer struct {
+	dotEosPattern   *regexp.Regexp
+	symboEosPattern *regexp.Regexp
+}
+
+func NewEnglishSentencizer() *EnglishSentencizer {
+	s := &EnglishSentencizer{
+		dotEosPattern:   regexp.MustCompile(_EN_DOT_EOS_PATTERN),
+		symboEosPattern: regexp.MustCompile(_EN_SYMBOL_EOS_PATTERN),
+	}
+
+	return s
+}
+
+func (s *EnglishSentencizer) Sentencize(tokenSeq []string) [][]string {
+	sentenceSeq := make([][]string, 0)
+	sentence := make([]string, 0)
+
+	for _, token := range tokenSeq {
+		sentence = append(sentence, token)
+
+		if s.dotEosPattern.MatchString(token) || s.symboEosPattern.MatchString(token) {
+			sentenceSeq = append(sentenceSeq, sentence)
+			sentence = make([]string, 0)
+			continue
+		}
+	}
+
+	sentenceSeq = append(sentenceSeq, sentence)
+
+	return SentenceSeq
+}
